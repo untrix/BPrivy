61295249ba795a92e0aa7a52b4ae0a16aef86b6a
Sumeet Singh <sumeet@untrix.com>
2012-03-19 22:49:17 -0700

    Revert "Revert "Element Linking Via. DND implemented.""
    This reverts commit 423a8b00a9797b2643ae67706c901df30739c10f.

--- Chrome Extension/BP_CS.js
+++ Chrome Extension/BP_CS.js
@@ -1,268 +1,260 @@
 /* Global declaration for JSLint */
 /*global document */
 
-// Private namespace in a Function Closure
-var bp = ( function() {
+// All code is in a block scope defined via. an anynymous function
+//(function() {
+
 	// Element IDs and Selectors
-	var panelId = "bpPanel";
-	var	panelSelector = "#" + panelId;
-	var panelTitleId = "bpPanelTitle";
-	var panelListId = "bpPanelList";
-
-	// CSS Class Names
-	var panelClass = "bp-panel";
-	var userInpClass = "bp-user-in";
-	var passInpClass = "bp-pass-in";
-	var outputClass  = "bp-out";
-	var userOutClass = "bp-user-out";
-	var passOutClass = "bp-pass-out";
-	var panelTitleClass = "bp-panel-title";
-	var containerClass = "bp-container";
-
-	var _f = {
-		"bpcPanelId" : function() {
-			return panelId;
-		},
-		
-		"bpcPanelSelector" : function() {
-			return panelSelector;
-		},
-		
-		"DoCreatePanel" : function() {
-			return (top == self);
-		},
-		
-		"IsDocVisible" : function(document) {
-			return ((document.webkitVisibilityState && (document.webkitVisibilityState === 'visible')) || ($(document.body).is(":visible")));
-		},
+	var bp_g =
+	{
+		"panelId": "bpPanel",
+		"panelTitleId":"bpPanelTitle",
+		"panelListId":"bpPanelList",
 	
-		"IsFrameVisible" : function(frame)
+		// CSS Class Names
+		"panelClass": "bp-panel",
+		"outputClass":"bp-out",
+		"userOutClass": "bp-user-out",
+		"passOutClass": "bp-pass-out",
+		"containerClass": "bp-container",
+		
+		//
+		"bpContentType": "x-bp-content",
+		"draggedElementID": null
+	};
+
+	function DoCreatePanel() {
+		return (top == self);
+	}
+	
+	function IsDocVisible(document) {
+		return ((document.webkitVisibilityState && (document.webkitVisibilityState === 'visible')) || ($(document.body).is(":visible")));
+	}
+
+	function IsFrameVisible(frame)
+	{
+		var retval = true;
+		console.info("Entered IsFrameVisible");
+		if (frame.hidden)
+			retval = false;
+		else if (!frame.style)
+			retval = true;
+		else// frame.style exists
 		{
-			var retval = true;
-			console.info("Entered IsFrameVisible");
-			if (frame.hidden)
+			if(frame.style.visibility && frame.style.visibility === 'hidden')
 				retval = false;
-			else if (!frame.style)
-				retval = true;
-			else// frame.style exists
+			else if(frame.style.display && frame.style.display === 'none')
+				retval = false;
+		}
+
+		console.info("Exiting IsFrameVisible");
+		return retval;
+	}
+
+	function preventDefault (e)
+	{
+		console.log("pd invoked");
+		e.preventDefault();
+		return false;
+	}
+	
+	function stopPropagation(e)
+	{
+		//console.info("stopPropagation invoked");
+		e.stopPropagation();
+	}
+	
+	function setupDNDWatchers()
+	{
+		function inputHandler(e)
+		{
+			if (bp_g.draggedElementID && ($("#"+bp_g.draggedElementID).val() === e.target.value))
 			{
-				if(frame.style.visibility && frame.style.visibility === 'hidden')
-					retval = false;
-				else if(frame.style.display && frame.style.display === 'none')
-					retval = false;
+				var name = $(document.getElementById(bp_g.draggedElementID)).attr("name");
+				//console.info("Linking elements " + bp_g.draggedElementID + "/" + name + " and " + e.target.id + "/" + e.target.name);
 			}
-
-			console.info("Exiting IsFrameVisible");
-			return retval;
-		},
-
-		"preventDefault": function (e)
-		{
-			console.log("pd invoked");
-			e.preventDefault();
-			return false;
-		},
+		}
 		
-		"stopPropagation": function (e)
-		{
-			console.info("stopPropagation invoked");
-			e.stopPropagation();
-			return true;
-		},
+		$("input").each(function(i, el) {
+			if (isUserid(el) || isPassword(el))
+			{
+				el.addEventListener("input", inputHandler, false);
+				console.log("Added event listener for element " + el.id + "/" + el.name);
+			}
+		});
+	}
+	
+	function createImageElement(imgPath)
+	{
+		var el = document.createElement("img");
+		el.src = chrome.extension.getURL(imgPath);
 		
-		"handleDragEnd": function (e)
-		{
-			console.info("DragEnd received");
-			return true;
-		},
-
-		"handleDragStart": function (e)
+		return el;
+	}
+	
+	function makeItemsDraggable(j_container)
+	{
+		function handleDragStart (e)
 		{
 			console.info("DragStartHandler entered");
 			//$("#bpPanel").draggable("destroy");
-			e.dataTransfer.effectAllowed = "copy";
+			e.dataTransfer.effectAllowed = "copyLink";
 			//e.dataTransfer.dropEffect = "copy";
 			var data = e.target.value;
-			console.info("setting data to " + data);
+			//console.info("setting data to " + data + " and bpcontent to " + e.target.id);
 			e.dataTransfer.setData('text/plain', data);
-			e.dataTransfer.setData('text/x-bp-content', e.target.id);
-			//e.dataTransfer.setDragImage(e.target, 0, 0);
-			e.stopPropagation();
-			console.info("DataTransfer.items = "+e.dataTransfer.items[0]);
-			console.info("tranfer data = " + e.dataTransfer.getData("text/plain"));
+			e.dataTransfer.setData(bp_g.bpContentType, e.target.id);
+			//e.dataTransfer.addElement(e.target);
+			e.dataTransfer.setDragImage(createImageElement("icon16.png"), 0, 0);
+			//e.stopPropagation();
+			//console.info("DataTransfer.items = "+ e.dataTransfer.items.toString());
+			//console.info("tranfer data = " + e.dataTransfer.getData("text/plain"));
+			bp_g.draggedElementID = e.target.id;
 			return true;
-		},
-		
-		"addDragListeners": function (j)
+		}
+
+		j_container[0].addEventListener('dragstart', handleDragStart, false);
+
+		function handleDragEnd(e)
 		{
-			j[0].addEventListener('dragstart', _f.handleDragStart, false);
-			j[0].addEventListener('dragend', _f.handleDragEnd, false);
-		},
-		
-		"InsertIOItem": function (jq, id, user, pass)
-		{
-			var userid = "username" + id;
-			var passid = "password" + id;
-			var j_li = $(document.createElement("li")).addClass(containerClass);
+			console.info("DragEnd received");
+			bp_g.draggedElementID = null;
+			return true;
+		}
 
-			var j_inu = $("<span></span>").attr(
-												{draggable: true,
-													//contenteditable: true,
-												 //required: true,
-												 id: userid, 
-												 value:user,
-												 //size: 12,
-												 //maxlength: 100
-												 }
-												).addClass(outputClass + " " + userOutClass).text(user);
-
-			var j_inp = $("<span></span>").attr(
-												{
-													draggable: true,
-													//contenteditable: true,
-												    //required: true, 
-													id: passid, 
-													value: pass,
-													//size:12,
-													//cols: 12, rows: 1,
-													//maxlength: 100,
-												}
-												).addClass(outputClass + " " + passOutClass).text("...");
-						
-			jq.append(j_li.append(j_inu).append(j_inp));
-
-			// Prevent mousedown to bubble up in order to prevent panel dragging by
-			// jquery-ui.
-			j_li[0].addEventListener('mousedown', _f.stopPropagation, false);
-		},
-		
-		"createPanelTitle": function () {
-			return $(document.createElement("div")).attr({
-						id: panelTitleId
-					}).text("BPrivy");
-		},
-
-		// CREATE THE CONTROL-PANEL
-		"CreatePanel": function()
-		{
-			var panelW, winW, left;
-			//var document = win.document;
-
-			var tmp_el = document.createElement("div");
-			var panel = $(tmp_el).attr('id', panelId).addClass(panelClass);
-			
-			var ul = $("<ul></ul>").attr("id", panelListId).addClass(containerClass);
-			
-			ul.append(_f.createPanelTitle());
-			_f.InsertIOItem(ul, "1", "username1", "password1");
-			_f.InsertIOItem(ul, "2", "user2", "passw2");
-			_f.addDragListeners(ul);
-
-			panel.append(ul);
-			
-			if (document.body) {
-				panel.hide().appendTo('body');
-			}
-			
-			// Make sure that postion:fixed is supplied at element level otherwise draggable() overrides it
-			// by setting position:relative. Also we can use right:0px here because draggable() does not like it.
-			// Hence we need to calculate the left value :(			
-			panelW = panel.outerWidth();
-			winW = document.body.clientWidth ? document.body.clientWidth :
-					$(document.body).innerWidth();
-			
-			left = (winW-panelW);
-			left = (left>0)? left: 0;
-			
-			console.info("WinW = " + winW + " panelW = " + panelW);
-
-			panel.css({position: 'fixed', top: '0px', 'left': left + "px"});
-
-			// Make it draggable after all above mentioned style properties have been applied to the element.
-			// Otherwise draggable() will override those properties.
-			panel.draggable();
-			
-			return panel;
-		},
-		
-		"GetPanel": function ()
-		{
-			var el = document.getElementById(bp.bpcPanelId());
-			if(el)
-				return $(el);
-			else
-				console.error("BPGetPanel could not find BPPanel");
-		},
-
-		"dropHandler": function(e)
-		{
-			console.info("DropHandler invoked");
-			var data = e.dataTransfer.getData("text/x-bp-content"); 
-			if (data)
-			{
-				console.info("Data from element " + data + " dropped at element " + e.target.id);
-			}
-		},
-		
-		"isUserid": function (el)
-		 {
-			 if (el.type)
-			 	return (el.type==="text" || el.type==="email" || el.type==="tel" || el.type==="url" || el.type==="number");
-			 else
-				 return true; // text type by default
-		 },
-		
-		 "isPassword": function (el)
-		 {
-		 	return (el.type === "password");
-		 },
-
-		"probe": function (e)
-		{
-			console.log("probe invoked");	
-		},
-		
- 		"setupDNDLinks": function()
- 		{
- 			$("input").each(function(i, e) {
- 				if (_f.isUserid(e) || _f.isPassword(e))
- 				{
- 					e.addEventListener("drop", _f.dropHandler, true);
- 					e.addEventListener("dragover", _f.probe, false);
- 					console.log("Added event listener for element " + e.id + "/" + e.name);
- 				}
- 			});
- 		},
- 		
-		"clickBP": function (request, sender, sendResponse)
-		{
-			// Only show the panel in the top-level frame.
-			if(bp.DoCreatePanel()) 
-			{
-				var p = bp.GetPanel();
-				if (p) p.toggle();
-			}
-		
-			sendResponse({});
-			return;
-		},
-		
-		"main": function()
-		{
-			if(bp.DoCreatePanel()) 
-			{
-				console.log("BP_CS entered on page " + location.href);
-				_f.CreatePanel();
-				_f.setupDNDLinks();
-				chrome.extension.onRequest.addListener(_f.clickBP);
-			}
-		} 		 
-	};
+		j_container[0].addEventListener('dragend', handleDragEnd, false);
+	}
 	
-	return _f;
-}() );
+	function insertIOItem (jq, id, user, pass)
+	{
+		var userid = "username" + id;
+		var passid = "password" + id;
+		var j_li = $(document.createElement("li")).addClass(bp_g.containerClass);
 
-bp.main();
+		var j_inu = $("<span></span>").attr(
+			{draggable: true,
+			 //contenteditable: true,
+			 //required: true,
+			 id: userid,
+			 name: userid,
+			 value:user,
+			 //size: 12,
+			 //maxlength: 100
+			 }
+			).addClass(bp_g.outputClass + " " + bp_g.userOutClass).text(user);
+
+		var j_inp = $("<span></span>").attr(
+			{
+				draggable: true,
+				//contenteditable: true,
+			    //required: true, 
+				id: passid,
+				name: userid,
+				value: pass,
+				//size:12,
+				//cols: 12, rows: 1,
+				//maxlength: 100,
+			}
+			).addClass(bp_g.outputClass + " " + bp_g.passOutClass).text("...");
+					
+		jq.append(j_li.append(j_inu).append(j_inp));
+
+		// Prevent mousedown to bubble up in order to prevent panel dragging by
+		// jquery-ui.
+		j_li[0].addEventListener('mousedown', stopPropagation, false);
+	}
+	
+    function createPanelTitle ()
+	{
+		return $(document.createElement("div")).attr({
+					id: bp_g.panelTitleId
+				}).text("BPrivy");
+	}
+
+	// CREATE THE CONTROL-PANEL
+	function CreatePanel()
+	{
+		var panelW, winW, left;
+		//var document = win.document;
+
+		var tmp_el = document.createElement("div");
+		var panel = $(tmp_el).attr('id', bp_g.panelId).addClass(bp_g.panelClass);
+		
+		var ul = $("<ul></ul>").attr("id", bp_g.panelListId).addClass(bp_g.containerClass);
+		
+		ul.append(createPanelTitle());
+		insertIOItem(ul, "1", "username1", "password1");
+		insertIOItem(ul, "2", "user2", "passw2");
+		// Attache dragStart and End listener to container in order to make its items draggable.
+		makeItemsDraggable(ul);
+
+		panel.append(ul);
+		
+		if (document.body) {
+			panel.hide().appendTo('body');
+		}
+		
+		// Make sure that postion:fixed is supplied at element level otherwise draggable() overrides it
+		// by setting position:relative. Also we can use right:0px here because draggable() does not like it.
+		// Hence we need to calculate the left value :(			
+		panelW = panel.outerWidth();
+		winW = document.body.clientWidth ? document.body.clientWidth :
+				$(document.body).innerWidth();
+		
+		left = (winW-panelW);
+		left = (left>0)? left: 0;
+		
+		console.info("WinW = " + winW + " panelW = " + panelW);
+
+		panel.css({position: 'fixed', top: '0px', 'left': left + "px"});
+
+		// Make it draggable after all above mentioned style properties have been applied to the element.
+		// Otherwise draggable() will override those properties.
+		panel.draggable();
+		
+		return panel;
+	}
+
+	function isUserid(el)
+	 {
+		 if (el.type)
+		 	return (el.type==="text" || el.type==="email" || el.type==="tel" || el.type==="url" || el.type==="number");
+		 else
+			 return true; // text type by default
+	 }
+	
+	 function isPassword (el)
+	 {
+	 	return (el.type === "password");
+	 }
+
+	function clickBP (request, sender, sendResponse)
+	{
+		// Only show the panel in the top-level frame.
+		if(DoCreatePanel()) 
+		{
+			$(document.getElementById(bp_g.panelId)).toggle();
+		}
+	
+		sendResponse({});
+		return;
+	}
+	
+	function main()
+	{
+		if(DoCreatePanel()) 
+		{
+			console.log("BP_CS entered on page " + location.href);
+			CreatePanel();
+			setupDNDWatchers();
+			chrome.extension.onRequest.addListener(clickBP);
+		}
+	} 		 
+
+	
+	main();
+//}());
+
 
 /*
  
@@ -276,15 +268,49 @@ bp.main();
  if (IsUserID(el))
  {
  el.value = "Username";
+			// {
  response[0] = {"username": "LoginID"};
  }
  else if (el.type === "password")
- {
  el.style = "background: red; color: blue";
  response[0] = {"username": "Password"};
  }
  }
  sendResponse(response);
  }
+
+		// "dropHandler": function(e)
+		// {
+			// console.info("DropHandler invoked");
+			// var data = e.dataTransfer.getData(bp_g.bpContentType); 
+			// if (data)
+				// console.info("Data from element " + data + " dropped at element " + e.target.id);
+			// }
+		// },
+		
+		// "dragoverHandler": function (e)
+		// {
+			// console.log("dragoverHandler invoked");
+			// var items = e.dataTransfer.items;
+			// var found = false;
+			// if (items)
+			// {
+				// for (var i = 0; i < items.length; i++)
+				// {
+					// if (items[i].type === bp_g.bpContentType)
+						// found = true;
+					// console.log(items[i].kind + ":" + items[i].type);
+				// }
+			// }
+// 			
+			// if (found)
+			// {
+				// console.info("found x-bp-content data hovering over element " + e.target.id + "/" + e.target.name); 
+				// e.dataTransfer.dropEffect = "copy";
+				// e.preventDefault();
+				// return false;
+			// }	
+		// },
+		
 
  */
