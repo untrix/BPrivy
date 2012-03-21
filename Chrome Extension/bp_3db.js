28df9572d849edf5ad040e9ea252563bd53bb80c
Sumeet Singh <sumeet@untrix.com>
2012-03-20 21:16:47 -0700

    syncing wip

---
+++ Chrome Extension/bp_3db.js
@@ -0,0 +1,22 @@
+/**
+ * @author Sumeet Singh
+ * @mail sumeet@untrix.com
+ * @copyright Copyright (c) 2012. All Right Reserved, Sumeet S Singh
+ */
+
+/* Global declaration for JSLint */
+/*global document */
+
+// constructs an event-record
+function constructERecord()
+{
+	var er = {};
+	var username = null;
+	var password = null;
+	var url = null;
+	var tag = null;
+	var tag_id = null;
+	var tag_name = null;
+}
+
+function decrpyt(pass) {return pass;}
