327806138f5ff732e8eba3570a74aff384711e54
Sumeet Singh <sumeet@untrix.com>
2012-03-21 13:53:07 -0700

    Fetching and Merging from remote

--- Chrome Extension/bp_3db.js
+++ Chrome Extension/bp_3db.js
@@ -7,16 +7,34 @@
 /* Global declaration for JSLint */
 /*global document */
 
-// constructs an event-record
-function constructERecord()
+// The following pattern defines a module, all who's contents are encolsed inside a
+// anonymous function scope. The returned object holds references to the public
+// functions and variables only. The remaining members are private to the module.
+function construct3dbCache(url) 
 {
-	var er = {};
-	var username = null;
-	var password = null;
-	var url = null;
-	var tag = null;
-	var tag_id = null;
-	var tag_name = null;
+	// Fields of an event-record
+	var fields = ['username', 'password', 'url', 'tag', 'tag_id', 'tag_name'];
+	
+	var saveRecord = function(o)
+	{
+		if (o && o.url && o.username)
+		{
+			var url = sanitizeUrl(o.url);
+			eRecords[url][o.username] = o;
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	var getRecord = function(url, username)
+	{
+		if (url && username)
+			return eRecords[url][username];
+	}
+	
+
+	return {"saveRecord": saveRecord,
+			"getRecord": getRecord};
 }
 
-function decrpyt(pass) {return pass;}
