0d162d4c521b7f16d3ca6082ff4cc945cce313e9
Sumeet Singh <sumeet@untrix.com>
2012-07-11 11:47:35 -0700

    Syncing WIP.

--- Plugin/plugin-win-src/BPrivy/Security.cpp
+++ Plugin/plugin-win-src/BPrivy/Security.cpp
@@ -1,17 +1,17 @@
 #include <boost/random/random_device.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
-
+/*
 int RandomPassword() {
-    1std::string chars(
+    std::string chars(
         "abcdefghijklmnopqrstuvwxyz"
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         "1234567890"
         "!@#$%^&*()"
         "`~-_=+[{]{\\|;:'\",<.>/? ");
-    2boost::random::random_device rng;
-    3boost::random::uniform_int_distribution<> index_dist(0, chars.size() - 1);
+    boost::random::random_device rng;
+    boost::random::uniform_int_distribution<> index_dist(0, chars.size() - 1);
     for(int i = 0; i < 8; ++i) {
         std::cout << chars[index_dist(rng)];
     }
     std::cout << std::endl;
-}
+}*/
